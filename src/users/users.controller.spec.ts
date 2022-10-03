04f878dafa78375b549c956b48ce580d0b245951
dev-t2 <oo_ri@naver.com>
2022-10-03 23:39:12 +0900

    Users Resource Install and Setup

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,6 +9,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
